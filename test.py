efb5af8755e246ec564eef60b1752a01f52ccac6
chenyonda2018 <1243724041@qq.com>
2019-06-23 10:03:56 +0800

    add 课件

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello")
+import requests
+import json
+url = 'https://api.github.com/some/endpoint'
+payload = {'some': 'data'}
+headers = {'content-type': 'application/json'}
+ret = requests.post(url, data=json.dumps(payload),headers=headers)
+print(ret.url)
+print(ret.text)
+
